f729ab9d1a0dbfc2f468868ed7c5a636d5941e65
Alyskys <Gunstavoflol@gmail.com>
2020-11-11 13:08:54 +0100

    Booking model

---
+++ RESTServices/Models/Booking.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+
+namespace RESTServices.Models {
+    public class Booking {
+
+        //public Account Account { get; set; } Implement when done
+        public bool PayedFor { get; set; }
+        public double PaymentAmount { get; set; }
+        //public Location StartLocation { get; set; }
+        //public Location EndLocation { get; set; }
+        public DateTime BookingDate { get; set; }
+        //public Car Car { get; set; }
+
+    }
+}
