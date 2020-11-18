4114ff563e3fd32d40a293a14fe26a208b90a0ac
Alyskys <Gunstavoflol@gmail.com>
2020-11-18 12:56:47 +0100

    Test project made. Account Crud tests added. Assert.AreEqual() not working :c

--- TestProjekt/AccountCrudTests.cs
+++ TestProjekt/AccountCrudTests.cs
@@ -8,33 +8,80 @@ using System.Configuration;
 namespace TestProjekt {
     [TestClass]
     public class AccountCrudTests {
-
-        private Account account = new Account() {
-            Name = "Ole",
-            Email = "OKOKOK@Gmail.com",
-            Phone = "31645121"
-        };
-        private AccountDB accountDB = new AccountDB();
+        private int accountId;
+        private Account account;
+        private AccountDB accountDB;
 
 
         [TestInitialize]
         public void StartSetup() {
+            account = new Account() {
+                Name = "Ole",
+                Email = "OKOKOK@Gmail.com",
+                Phone = "31645121"
+            };
+            accountDB = new AccountDB();
+            accountId = (int) accountDB.Create(account);
 
         }
 
         [TestMethod]
-        public void CreateTest() {
+        public void Test10_Create() {
 
-            accountDB.Create(account);
-            using(SqlConnection con = new SqlConnection(ConfigurationManager)
-            accountDB.Get();
+            Account accountFromDB;
+            accountFromDB = accountDB.Get(accountId);
+            Console.WriteLine("CREATE METHOD" + "Account Id = " + accountId);
+            account.Id = accountId;
+            Console.WriteLine(account.Id + account.Name + account.Email + account.Phone);
+            Console.WriteLine(accountFromDB.Id + accountFromDB.Name + accountFromDB.Email + accountFromDB.Phone);
 
-
-            Assert.AreEqual(account)
+            Assert.AreEqual(account, accountFromDB);
+            //Assert.IsTrue(account.Equals(accountFromDB));
         }
+
         [TestMethod]
-        public void GetTest() {
+        public void Test20_Get() {
+
+            Account accountFromDB;
+            Console.WriteLine("GET METHOD " + accountId);
+            accountFromDB = accountDB.Get(accountId);
+
+            Assert.AreEqual(account, accountFromDB);
+            //Assert.IsTrue(account.Equals(accountFromDB));
+
 
         }
+
+        [TestMethod]
+
+        public void Test30_Update() {
+            Account updateAccount = new Account() {
+                Id = accountId,
+                Name = "Ikke Ole",
+                Email = "NyEmail@gmail.com",
+                Phone = "15469712",
+            };
+            accountDB.Update(updateAccount);
+            Account accountFromDB = accountDB.Get(accountId);
+            Assert.AreNotEqual(account, accountFromDB);
+
+        }
+
+        [TestMethod]
+        public void Test40_Delete() {
+            Account accountFromDB = null;
+            accountDB.Delete(accountId);
+            try {
+            accountFromDB = accountDB.Get(accountId);
+
+            }
+            catch (InvalidOperationException e) {
+
+            }
+
+            Assert.IsNull(accountFromDB);
+
+        }
+
     }
 }
