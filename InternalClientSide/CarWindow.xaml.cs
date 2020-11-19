8a0c983fa5c4336b1951b13449c6fe1d6c9a140f
Alyskys <Gunstavoflol@gmail.com>
2020-11-19 13:11:21 +0100

    Home screen gui for internal application. Very bare bones and will probably need to be revisited

--- InternalClientSide/CarWindow.xaml.cs
+++ InternalClientSide/CarWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using InternalClientSide.Model;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,18 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
+using InternalClientSide.Model;
 
 namespace InternalClientSide {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for CarWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window {
-
-        public MainWindow() {
+    public partial class CarWindow : Window {
+        public CarWindow() {
             InitializeComponent();
+            carsGrid.DataContext = Car.GetCars();
         }
-
     }
 }
